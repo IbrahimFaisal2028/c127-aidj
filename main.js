66160301a5e3e9d7f192e4b8acfc3f68a52ecfce
IbrahimFaisal2028 <108027026+IbrahimFaisal2028@users.noreply.github.com>
2023-04-29 19:43:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
 function preload()
 {
 	song = loadSound("music.mp3");
@@ -15,14 +16,24 @@ function setup() {
 	video = createCapture(VIDEO);
 	video.hide();
 
-	posenET = ml5.poseNet(video,modelLoaded);
+	poseNet = ml5.poseNet(video,modelLoaded);
 	poseNet.on('pose', gotPoses);
 
 }
 
 function draw() {
 	image(video, 0, 0, 600, 500);
-
+	fill("#FF0000");
+	stroke("#FF0000");
+if(scoreLeftWrist > 0.2)
+{
+	circle(leftWristX,leftWristY,20);
+	InNumberleftWristY = Number(leftWristY);
+	remove_decimals = floor(InNumberleftWristY);
+	volume = remove_decimals/500;
+	document.getElementById("volume").innerHTML = "Volume = " + volume;
+	song.setVolume(volume);
+}
 }
 
 function play()
@@ -42,6 +53,8 @@ function gotPoses(results)
 		console.log(results);
 		leftWristX = results[0].pose.leftWrist.x;
 		leftWristY = results[0].pose.leftWrist.y;
+		scoreLeftWrist = results[0].pose.keypoints[9].score;
+		console.log("scoreLeftWrist = " + scoreLeftWrist);
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
 		rightWristX = results[0].pose.rightWrist.x;
 		rightWristY = results[0].pose.rightWrist.y;
